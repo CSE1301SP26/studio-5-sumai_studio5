f4ee29ba1d9a1e77858eb97ed867d93860677fdb
Jencysu <jency@wustl.edu>
2026-02-25 15:22:44 -0600

    all done

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,7 +16,7 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt((Math.pow((x2 - x1),2))+(Math.pow((y2 - y1),2)));
 		return distance;
 	}
 
@@ -75,7 +75,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for(int i = 0; i < values.length; i++){
+			sum +=  values[i];
+		}
 		return sum;
 	}
 
@@ -87,10 +89,11 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
-
-		
+		for(int i = 0; i < length; i++){
+			values[i] = value;
+		}
 
 		return values;
 	}
@@ -98,5 +101,9 @@ public class Methods {
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
 
-	
+	public static double arrayMean(int[] value) {
+		double sum = arraySum(value);
+		double avg = sum/value.length;
+		return avg;
+    }
 }
